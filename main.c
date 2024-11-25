7eede1ca6e00a984cbb57c43ee476de0a27b2043
Elizabeth Chen <echen50@stuy.edu>
2024-11-25 15:35:21 -0500

    started forking

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+
+int generate_rand()
+int main() {
+  int pid = getpid();
+  printf("%d about to create 2 child processes.\n", pid);
+  printf("");
+  pid_t child1;
+  pid_t child2;
+  child1 = fork();
+  child2 = fork();
+  if (child1 < 0 || child2 < 0) {
+    perror("did not fork");
+  }
+  else if (child1 == 0) {
+
+  }
+  else {
+
+  }
+
+}
