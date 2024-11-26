afb8d679ecd07e6ddababa7a36a87b5120bf6cd1
Elizabeth Chen <echen50@stuy.edu>
2024-11-26 16:07:37 -0500

    finished lab

--- main.c
+++ main.c
@@ -5,25 +5,44 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <time.h>
-#include <sys/types.h>
+#include <sys/wait.h>
 
-int generate_rand()
+void child_proc() {
+  int child_pid = getpid();
+  srand(child_pid);
+  int num = 1 + rand() % 5;
+  printf("%d %dsec\n", child_pid, num);
+  sleep(num);
+  printf("%d finished after %dsec\n", child_pid, num);
+  exit(num);
+}
 int main() {
   int pid = getpid();
-  printf("%d about to create 2 child processes.\n", pid);
-  printf("");
+  printf("%d about to create 2 child processes\n", pid);
   pid_t child1;
-  pid_t child2;
   child1 = fork();
-  child2 = fork();
-  if (child1 < 0 || child2 < 0) {
-    perror("did not fork");
+  if (child1 > 0) { // parent
+    pid_t child2;
+    child2 = fork();
+    if (child2 < 0) {
+      perror("did not fork\n");
+    }
+    else if (child2 == 0) {
+      child_proc();
+    }
+    else {
+    int status;
+    int child_pid = wait(&status);
+    int sleep = WEXITSTATUS(status);
+    printf("Main Process %d is done. Child %d slept for %dsec\n", pid, child_pid, sleep);
   }
-  else if (child1 == 0) {
-
+}
+  else if (child1 < 0) {
+    perror("did not fork\n");
   }
   else {
-
+    child_proc();
   }
+
 
 }
